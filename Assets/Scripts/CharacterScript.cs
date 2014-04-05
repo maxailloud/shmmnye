50ad903efd99e0b2cdc744356ed21a9aefb6173a
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-04-05 17:04:02 +0200

    Changing how to add score ever seconds

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -5,120 +5,122 @@ public class CharacterScript : MonoBehaviour
 {
 
 	/// <summary>
-	/// 1 - The type of the character
-	/// </summary>
-	public bool isEnemy = false;
+		/// 1 - The type of the character
+		/// </summary>
+		public bool isEnemy = false;
 
-	/// <summary>
-	/// 2 - The movement speed of the character
-	/// </summary>
-	public Vector3 staticMovement;
-	public Vector3 movement;
+		/// <summary>
+		/// 2 - The movement speed of the character
+		/// </summary>
+		public Vector3 staticMovement;
+		public Vector3 movement;
 
-	public int lineCharacter;
-	public int lineEnnemy;
-	
-	public int boost = 0;
+		public int lineCharacter;
+		public int lineEnnemy;
+		
+		public int boost = 0;
 
-	public float score = 0;
-	public float points = 1.0f;
-	//public int multiplier = 1;
+		public float score = 0;
+		public float increaseScore = 1;
+		//public int multiplier = 1;
 
 		/// <summary>
 		/// 3 - Line on which the character is
 		/// </summary>
 		public int line = 3;
 
-	// Use this for initialization
-	void Start () 
-	{
-		print (isEnemy);
-		if (!isEnemy) 
+		// Use this for initialization
+		void Start () 
 		{
-			staticMovement = new Vector3 (-ConstantScript.RUNNER_SPEED, 0, 0);
-		}
-		else
-		{
-			staticMovement = new Vector3 (-ConstantScript.ENNEMY_SPEED, 0, 0);
-
-			transform.position.Set(10.0f, -2.512199f, 0.0f);
-
-			lineEnnemy = Random.Range(1, 6);
-
-			switch(lineEnnemy)
+			print (isEnemy);
+			if (!isEnemy) 
 			{
-				case 1 :
-				transform.Translate(0.0f, 0.0f, 0.0f);
-				break;
+				staticMovement = new Vector3 (-ConstantScript.RUNNER_SPEED, 0, 0);
+			}
+			else
+			{
+				staticMovement = new Vector3 (-ConstantScript.ENNEMY_SPEED, 0, 0);
 
-				case 2 :
-				transform.Translate(0.0f, 0.8f, 0.0f);
-				break;
+				transform.position.Set(10.0f, -2.512199f, 0.0f);
 
-				case 3 :
-				transform.Translate(0.0f, 1.7f, 0.0f);
-				break;
+				lineEnnemy = Random.Range(1, 6);
 
-				case 4 :
-				transform.Translate(0.0f, 2.2f, 0.0f);
-				break;
+				switch(lineEnnemy)
+				{
+					case 1 :
+					transform.Translate(0.0f, 0.0f, 0.0f);
+					break;
 
-				case 5 :
-				transform.Translate(0.0f, 2.6f, 0.0f);
-				break;
+					case 2 :
+					transform.Translate(0.0f, 0.8f, 0.0f);
+					break;
+
+					case 3 :
+					transform.Translate(0.0f, 1.7f, 0.0f);
+					break;
+
+					case 4 :
+					transform.Translate(0.0f, 2.2f, 0.0f);
+					break;
+
+					case 5 :
+					transform.Translate(0.0f, 2.6f, 0.0f);
+					break;
+				}
+			}
+
+			InvokeRepeating("addScore", 1.0f, 1.0f);
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{	
+			if (isEnemy) 
+			{
+				Vector3 mov = new Vector3 (
+					-ConstantScript.RUNNER_SPEED * Time.deltaTime,
+					0,
+					0);
+			
+				transform.Translate (mov);
+				return;
+			}
+
+			float shiftY = 0;
+
+			if (Input.GetKeyUp (KeyCode.UpArrow)) {
+					shiftY = changeLine (1);
+			} else if (Input.GetKeyUp (KeyCode.DownArrow)) {
+					shiftY = changeLine (-1);
+			}
+
+			float modif = 0;
+			if (boost > 0) {
+				modif = ConstantScript.BOOST_SPEED;
+				boost--;
+			}else if ( boost < 0) {
+				modif = -ConstantScript.BOOST_SPEED;
+				boost++;
+			}
+
+			// Update 1 - Move the character
+			Vector3 mov2 = new Vector3 (
+				(-ConstantScript.RUNNER_SPEED + modif) * Time.deltaTime,
+				shiftY,
+				0);
+
+			transform.Translate (mov2);
+
+
+			if (!renderer.IsVisibleFrom(Camera.main)) 
+			{
+				Application.LoadLevel("death");
 			}
 		}
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{	
-		if (isEnemy) 
-		{
-			Vector3 mov = new Vector3 (
-				-ConstantScript.RUNNER_SPEED * Time.deltaTime,
-				0,
-				0);
-		
-			transform.Translate (mov);
-			return;
+
+		void addScore () {
+			score += increaseScore;
 		}
-
-		float shiftY = 0;
-
-		if (Input.GetKeyUp (KeyCode.UpArrow)) {
-				shiftY = changeLine (1);
-		} else if (Input.GetKeyUp (KeyCode.DownArrow)) {
-				shiftY = changeLine (-1);
-		}
-
-		float modif = 0;
-		if (boost > 0) {
-			modif = ConstantScript.BOOST_SPEED;
-			boost--;
-		}else if ( boost < 0) {
-			modif = -ConstantScript.BOOST_SPEED;
-			boost++;
-		}
-
-		// Update 1 - Move the character
-		Vector3 mov2 = new Vector3 (
-			(-ConstantScript.RUNNER_SPEED + modif) * Time.deltaTime,
-			shiftY,
-			0);
-
-		transform.Translate (mov2);
-
-
-		if (!renderer.IsVisibleFrom(Camera.main)) 
-		{
-			print("Mort");
-			Application.LoadLevel("death");
-		}
-
-		score += points * Time.time;
-		print ((int)score);
-	}
 
 		float changeLine (int direction)
 		{
@@ -175,12 +177,12 @@ public class CharacterScript : MonoBehaviour
 					case "Speed" :
 						Debug.Log("Speed");
 						boost = ConstantScript.BOOST_LENGTH;
-					    score += points + 0.1f * Time.time;
+						score += increaseScore + increaseScore * Time.time;
 						break;
 					case "LSD" :
 						Debug.Log("LSD");
 						boost = -ConstantScript.BOOST_LENGTH/2;
-						score += points + 0.1f * Time.time;
+						score += increaseScore + increaseScore * Time.time;
 						break;
 					default :
 						Debug.Log("WARNING !!! should never happen !!! drug has no type");
