d562b5eaa22ef113c2dfec8c650858614d886963
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-04-05 13:15:48 +0200

    Use constant for line and start shifting line (not working)

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -1,37 +1,97 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CharacterScript : MonoBehaviour {
+public class CharacterScript : MonoBehaviour
+{
 
-	/// <summary>
-	/// 1 - The type of the character
-	/// </summary>
-	public bool isEnemy = false;
+		/// <summary>
+		/// 1 - The type of the character
+		/// </summary>
+		public bool isEnemy = false;
 
-	/// <summary>
-	/// 2 - The movement speed of the character
-	/// </summary>
-	public Vector2 speed;
+		/// <summary>
+		/// 2 - The movement speed of the character
+		/// </summary>
+		public Vector2 speed;
 
-	// Use this for initialization
-	void Start () {
-//		speed = new Vector2(-1, 0);
-	}
+		/// <summary>
+		/// 3 - Line on which the character is
+		/// </summary>
+		public int line = 1;
 	
-	// Update is called once per frame
-	void Update () {
-		// Update 1 - Move the character
-		Vector3 movement = new Vector3(
-			-ConstantScript.RUNNER_SPEED,
-			0,
-			0);
-		
-		movement *= Time.deltaTime;
-		transform.Translate(movement);
-	}
+		// Use this for initialization
+		void Start ()
+		{
+//		speed = new Vector2(-1, 0);
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{	
+				float shiftY = 0;
 
-	void OnTriggerEnter2D(Collider2D otherCollider)
-	{
-		Debug.Log("pouet");
-	}
+				if (Input.GetKey (KeyCode.UpArrow)) {
+						shiftY = changeLine (1);
+				} else if (Input.GetKey (KeyCode.DownArrow)) {
+						shiftY = changeLine (-1);
+				}
+
+				// Update 1 - Move the character
+				Vector3 movement = new Vector3 (
+					-ConstantScript.RUNNER_SPEED,
+					shiftY,
+					0);
+		
+				movement *= Time.deltaTime;
+				transform.Translate (movement);
+		}
+
+		void OnTriggerEnter2D (Collider2D otherCollider)
+		{
+				Debug.Log ("pouet");
+		}
+
+		float changeLine (int direction)
+		{
+		Debug.Log (direction);
+				float shiftY = 0;
+				float currentLineY = getLineY (line);
+				if (1 == direction && 5 > line) {
+						line++;
+						shiftY = getLineY (line) - currentLineY;
+				} else if (-1 == direction && 1 < line) {
+						line--;
+						shiftY = getLineY (line) - currentLineY ;
+				}
+				Debug.Log (shiftY);
+
+				return shiftY;
+		}
+
+		float getLineY (int line)
+		{
+				float y = 0;
+
+				switch (line) {
+				case 1:
+						y = ConstantScript.LINE_1;
+						break;
+				case 2:
+						y = ConstantScript.LINE_2;
+						break;
+				case 3:
+						y = ConstantScript.LINE_3;
+						break;
+				case 4:
+						y = ConstantScript.LINE_4;
+						break;
+				case 5:
+						y = ConstantScript.LINE_5;
+						break;
+				default:
+						break;
+				}
+
+				return y;
+		}
 }
--- Assets/Scripts/ConstantScript.cs
+++ Assets/Scripts/ConstantScript.cs
@@ -5,4 +5,11 @@ public class ConstantScript {
 
 	public const float TRACK_SPEED = 4;
 	public const float RUNNER_SPEED = 1;
+
+	// Position of the 5 rows
+	public const float LINE_1 = 0.0f;
+	public const float LINE_2 = 1.0f;
+	public const float LINE_3 = 1.8f;
+	public const float LINE_4 = 2.35f;
+	public const float LINE_5 = 2.7f;
 }
