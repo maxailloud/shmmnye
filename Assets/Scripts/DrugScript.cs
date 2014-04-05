94f2a2b53abf0c66421fecb6c0a9bfeb05587c77
Evan99 <evan.perret@gmail.com>
2014-04-05 11:16:38 +0200

    random drugs generation

--- Assets/Scripts/DrugScript.cs
+++ Assets/Scripts/DrugScript.cs
@@ -7,14 +7,51 @@ public class DrugScript : MonoBehaviour {
 	/// 1 - The type of the drug
 	/// </summary>
 	public string type = null;
+	public int line = 1;
+	public Vector2 speed = new Vector2(2, 2);			// Vitesse
+	public Vector2 direction = new Vector2(-1, 0);		// Sens de déplacement vers la gauche
+	private Vector2 movement;							// Position à chaque frame
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		// On tire une ligne entre 1 et 5 (Si les paramètres sont des entiers, range retourne des entiers).
+		line = Random.Range(1, 6);
+
+		switch (line) 
+		{
+			//On fait démarrer la drogue sur une ligne particulière
+			case 1 :
+				transform.position.Set(6.604088f, -3.544878f, 0.0f);
+				transform.Translate(1.98f, 0.0f, 0.0f);
+				break;
+			case 2 :
+				transform.position.Set(6.604088f, -3.544878f, 0.0f);
+				transform.Translate(1.98f, 1.0f, 0.0f);
+				break;
+			case 3 :
+				transform.position.Set(6.604088f, -3.544878f, 0.0f);
+				transform.Translate(1.98f, 1.8f, 0.0f);
+				break;
+			case 4 :
+				transform.position.Set(6.604088f, -3.544878f, 0.0f);
+				transform.Translate(1.98f, 2.35f, 0.0f);
+				break;
+			case 5 :
+				transform.position.Set(6.604088f, -3.544878f, 0.0f);
+				transform.Translate(1.98f, 2.7f, 0.0f);
+				break;
+		}
+
+		//Calcul du mouvement
+		movement = new Vector2(speed.x * direction.x, speed.y * direction.y);
+		Destroy(gameObject, 20); // 20sec
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		//On met à jour la position du Rigib Body 2D
+		rigidbody2D.velocity = movement;
 	}
 }
--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameScript : MonoBehaviour {
-
+public class GameScript : MonoBehaviour 
+{
+	public Transform drugPrefab;
+	private float drugsTime;
+	public float drugsCoolDown;			//Valeur qui devra évoluer dans la vie du jeu pour faire apparaitre des drogues plus vite
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		drugsTime = 0.0f;
+		drugsCoolDown = 1.0f;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if (drugsTime <= 0.0f) 
+		{
+			//Faire apparaitre les drogues ici
+			drugsTime += drugsCoolDown;
+			var drugTransform = Instantiate(drugPrefab) as Transform;
+		}
+		else if (drugsTime > 0.0f) 
+		{
+			drugsTime -= Time.deltaTime;
+		}
+
 	}
 }
