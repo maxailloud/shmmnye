9cf63d3dcd2c62816fa86251b78094a4381c5b63
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-04-05 10:15:26 +0200

    Adding scrolling script for moving track + adding constant script

---
+++ Assets/Scripts/ConstantScript.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantScript {
+
+	public const int TRACK_SPEED = 4;
+}
---
+++ Assets/Scripts/ScrollingScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Parallax scrolling script that should be assigned to a layer
+/// </summary>
+public class ScrollingScript : MonoBehaviour
+{	
+	void Update()
+	{
+		// Movement
+		Vector3 movement = new Vector3(
+			-ConstantScript.TRACK_SPEED,
+			0,
+			0);
+		
+		movement *= Time.deltaTime;
+		transform.Translate(movement);
+	}
+}
