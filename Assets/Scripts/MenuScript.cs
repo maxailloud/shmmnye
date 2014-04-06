5458b7409c1cf37f496803d8c1626bf1ada7b440
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-04-06 14:38:55 +0200

    Button on menu/credit/death page work

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -13,8 +13,5 @@ public class MenuScript : MonoBehaviour
 // Update is called once per frame
     void Update ()
     {
-        if (Input.anyKeyDown) {
-            Application.LoadLevel ("game");
-        }
     }
 }
