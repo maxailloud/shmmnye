ea8b828db3e4e9464eb548e8a8f32529fc92dce0
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-04-21 19:36:08 +0200

    Display count used drugs in death scene

--- Assets/Scripts/DeathSceneScript.cs
+++ Assets/Scripts/DeathSceneScript.cs
@@ -10,16 +10,35 @@ public class DeathSceneScript : MonoBehaviour {
 
     public TextMesh drugLevelText;
     public TextMesh drugLevelText3D;
+    
+    public TextMesh LSDText;
+    public TextMesh LSDText3D;
+    
+    public TextMesh SpeedText;
+    public TextMesh SpeedText3D;
+    
+    public TextMesh WaterText;
+    public TextMesh WaterText3D;
+    
+    public TextMesh EnemyText;
+    public TextMesh EnemyText3D;
 
 	// Use this for initialization
 	void Start () {
         scoreText.text   = "Score: " + CharacterScript.score;
         scoreText3D.text = "Score: " + CharacterScript.score;
+        
+        LSDText.text = "LSD: " + CharacterScript.nbLsd;
+        LSDText3D.text = "LSD: " + CharacterScript.nbLsd;
 
-//        scoreText3D.text = "Score: " + CharacterScript.nbLsd;
-//        scoreText3D.text = "Score: " + CharacterScript.nbSpeed;
-//        scoreText3D.text = "Score: " + CharacterScript.nbWater;
-//        scoreText3D.text = "Score: " + CharacterScript.nbEnemyHit;
+        SpeedText.text = "Speed: " + CharacterScript.nbSpeed;        
+        SpeedText3D.text = "Speed: " + CharacterScript.nbSpeed;
+
+        WaterText.text = "Eau: " + CharacterScript.nbWater;
+        WaterText3D.text = "Eau: " + CharacterScript.nbWater;
+
+        EnemyText.text = "Ennemies: " + CharacterScript.nbEnemyHit;
+        EnemyText3D.text = "Ennemies: " + CharacterScript.nbEnemyHit;
 
         distanceText.text   = "Distance: " + ScrollingScript.distance + "m";
         distanceText3D.text = "Distance: " + ScrollingScript.distance + "m";
